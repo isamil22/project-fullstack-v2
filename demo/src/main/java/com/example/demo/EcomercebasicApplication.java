01f7f38cf183bab48fa386982d565f44e610b160
isamil22 <yesofcourse2024@gmail.com>
2025-06-15 15:47:08 +0100

    fix prolbel of images gallery and iplod max

--- demo/src/main/java/com/example/demo/EcomercebasicApplication.java
+++ demo/src/main/java/com/example/demo/EcomercebasicApplication.java
@@ -2,14 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.mapstruct.MapperScan;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
 @SpringBootApplication
-//@MapperScan("com.example.demo.mapper")  // Add this line to enable MapStruct scanning
-
-
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO) // Add this annotation
 public class EcomercebasicApplication {
 
 	public static void main(String[] args) {
